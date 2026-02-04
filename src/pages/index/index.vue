e500f51023aefaf8484397ba630d03a68123ccd4
aaron <china.jinshuo@gmail.com>
2026-02-04 15:18:59 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>
+    <uni-badge text="1"></uni-badge>
+    <uni-badge text="2" type="success"></uni-badge>
+    <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello')
+</script>
+
+<style lang="scss"></style>
